3bcf40e23fabc61da95512babea40056386b0d03
alessandro-barate <alebarat@gmail.com>
2024-06-04 15:39:19 +0100

    [UPD] Cards generated

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <script>
 import MainComponent from './components/MainComponent.vue';
-import { store } from './store.js';
-import axios from 'axios';
 export default {
     name: 'RickMorthyApp',
     components: {
         MainComponent,
     },
 
-data(){
-    return {
-        store,
-    }
-},
-
-created(){
-    console.log('chiamata api');
-
-    axios.get(this.store.apiUrl).then((response) => {
-        this.store.apiResults = response.data.results;
-        console.log(this.store.apiResults);
-    });
-    
-    },
 };
 </script>
 <template>
-    <div v-for="character in store.apiResults" >{{ character.name }}</div>
-<MainComponent/>
+    <MainComponent/>
 </template>
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,16 +1,34 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
 export default {
     name: 'CardComponent',
+
+data(){
+    return {
+        store,
+    }
+},
+
+created(){
+    console.log('chiamata api');
+
+    axios.get(this.store.apiUrl).then((response) => {
+        this.store.apiResults = response.data.results;
+        console.log(this.store.apiResults);
+    });
+    
+    },
 };
 </script>
 
 <template>
     <div class="cards-container">
-        <div class="card">
+        <div v-for="character in store.apiResults" class="card">
             <img src="" alt="">
-            <p>nome</p>
-            <p>stato</p>
-            <p>razza</p>
+            <p>{{ character.name }}</p>
+            <p>{{ character.status }}</p>
+            <p>{{ character.species }}</p>
         </div>
     </div>
 </template>
