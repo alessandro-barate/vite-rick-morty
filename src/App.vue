e069bf5b1cba1a059e80e124de2c7de496a18e7e
alessandro-barate <alebarat@gmail.com>
2024-06-04 15:34:30 +0100

    [UPD] Api call working

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,14 @@ created(){
     console.log('chiamata api');
 
     axios.get(this.store.apiUrl).then((response) => {
-        this.store.apiResults = response.results;
+        this.store.apiResults = response.data.results;
+        console.log(this.store.apiResults);
     });
     
     },
 };
 </script>
 <template>
+    <div v-for="character in store.apiResults" >{{ character.name }}</div>
 <MainComponent/>
 </template>
